290741e385f9c8c91debb9dbd763e66e678c3c03
RedFlame <53618581+huynhat30@users.noreply.github.com>
2023-08-01 02:52:30 +0100

    add basic React components and html elements

--- calculator.js
+++ calculator.js
@@ -1,7 +1,40 @@
+
+const KeysDigit = () => {
+    const digits = [];
+    const digitID = ['one','two','three','four','five','six','seven','eight','nine']
+
+    for (let i = 1; i <10; i++) {
+        digits.push(
+                <button id={digitID[i-1]}>{i}</button>
+         )
+    }
+    return digits;
+}
+
 const App = () => {
     return (
-        <div>
-            <button id = "equals">=</button>
+        <div id="calculator">
+            <div id="display">
+                <span>(0)</span> 0
+            </div>
+
+            <div id="operators">
+                <button id = "add">+</button>
+                <button id = "subtract">-</button>
+                <button id = "multiply">*</button>
+                <button id = "divide">/</button>
+
+                <button id = "deleteAll">AC</button>
+                
+            </div>
+
+            <div id="keysDigit">
+                <KeysDigit />
+                <button id = "zero">0</button>
+                <button id = "decimal">.</button>
+                <button id = "equals">=</button>
+            </div>
+            
         </div>
     )
 }
